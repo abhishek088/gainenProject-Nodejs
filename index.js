f5c42cf355004e8bda50c9125a7a92b6abfa75b1
abhsihek088 <sawantabhishek08@gmail.com>
2021-03-09 12:38:50 -0500

    init

--- index.js
+++ index.js
@@ -43,17 +43,33 @@ const User = mongoose.model('User', {
     phone: String
 });
 
+const Idea = mongoose.model('Idea', {
+    title: String,
+    idea: String,
+    username: String,
+    isPostOnPublic: Boolean
+});
+
 //for refernce
-//home page
 // app.get('/', function(req, res){
 //     res.send('Hello world!');
 // });
 
+//home page get and post
 app.get('/', function(req, res){
     res.render('home');
 });
 
 app.post('/', function(req, res){
+    res.render('home');
+});
+
+//login page get and post
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+app.post('/login', function(req, res){
     var username = req.body.username;
     var password = req.body.password;
 
@@ -63,27 +79,77 @@ app.post('/', function(req, res){
     User.findOne({username: username, password: password}).exec(function(err, user){
             console.log(err);
 
-            req.session.username = user.username;
-            req.session.userLoggedIn = true;
-            res.redirect('/userprofile');
+            if(user){
+                req.session.username = user.username;
+                req.session.userLoggedIn = true;
+                res.redirect('/userprofile');
+            }
+            else{
+                res.render('login', {
+                    error: 'Incorrect username or password'
+                });
+            }
+            
         });
 });
 
 //user profile get and post
 app.get('/userprofile', function(req, res){
-    // User.find({username: username}).exec(function(err, users){
-    //     console.log(err);
-    //     res.render('userprofile', {users: users});
-    // });
-
-
-    //needs fix
-    
-    res.render('userprofile');
+    if(req.session.userLoggedIn){
+        User.findOne({username: req.session.username}).exec(function(err, user){
+            console.log(err)
+            res.render('userprofile', {
+                username: user.username,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                emailId: user.firstName,
+                phone: user.phone
+            });
+        });
+    }
+    else{
+        res.redirect('/login');
+    } 
 });
 
 app.post('/userprofile', function(req, res){
     res.render('userprofile');
+}); //post method not needed till edit action is created
+
+//logout get
+app.get('/logout', function(req, res) {
+    req.session.username = ''; 
+    req.session.userLoggedIn = false; 
+    User.findOne({username: req.session.username}).exec(function(err, user){
+        console.log(err)
+        res.render('login', {
+            error: 'Logout successful'
+        });
+    });
+});
+
+app.post('/logout', function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    console.log(username);
+    console.log(password);
+
+    User.findOne({username: username, password: password}).exec(function(err, user){
+            console.log(err);
+
+            if(user){
+                req.session.username = user.username;
+                req.session.userLoggedIn = true;
+                res.redirect('/userprofile');
+            }
+            else{
+                res.render('login', {
+                    error: 'Incorrect username or password'
+                });
+            }
+            
+        });
 });
 
 //register get and post
@@ -118,6 +184,65 @@ app.post('/register', function(req, res){
 
     res.render('register', registerData);
 });
+
+//post idea get  and post
+app.get('/postIdea', function(req, res){
+    if(!req.session.userLoggedIn)
+        res.redirect('/login');
+    else
+        res.render('postIdea');
+});
+
+app.post('/postIdea', function(req, res){
+    var title = req.body.title;
+    var idea = req.body.idea;
+    var isPublic = req.body.isPublic;
+
+    if(req.session.userLoggedIn){
+        User.findOne({username: req.session.username}).exec(function(err, user){
+            console.log(err)
+
+            if(isPublic == "public")
+                var isPostOnPublic = true;
+            else
+                var isPostOnPublic = false;
+
+            var ideaData = {
+                title: title,
+                idea: idea,
+                username: user.username,
+                isPostOnPublic: isPostOnPublic
+            }
+        
+            //store user
+            var newIdea = new Idea(ideaData);
+        
+            //save user
+            newIdea.save().then(function(){
+                console.log('new idea saved');
+            });
+        
+            res.render('postIdea', ideaData);
+        });
+    }
+    // else{
+    //     res.redirect('/login');
+    // }
+});
+
+//all ideas get and post
+app.get('/publicIdea', function(req, res){
+    Idea.find({}).exec(function(err, ideas){
+        console.log(err)
+
+        res.render('publicIdea', {ideas: ideas});
+    });
+});
+
+app.post('/publicIdea', function(req, res){
+    //dosomething
+});
+
 
 //listen to port
 app.listen(8080);
