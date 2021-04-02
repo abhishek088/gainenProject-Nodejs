36b601502d1c6a168720e95b1e48c44154a99bf2
Stephen Francis <schintalapudi2453@conestoga.on.ca>
2021-04-02 01:32:35 -0400

    delete/edit idea pages, like/del/edit idea buttons, favicon

--- index.js
+++ index.js
@@ -730,5 +730,5 @@ app.get('/adminLoggedInExplore', function (req, res) {
 
 //listen to port
 // app.listen(process.env.PORT, '0.0.0.0');
-app.listen(8080);
-console.log(`server is running on port 8080`);
+app.listen(8000);
+console.log(`server is running on port 8000`);
